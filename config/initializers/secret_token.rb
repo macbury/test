03ef0a381219f34b3e2009ae1941a4356c64178f
macbury <macbury@gmail.com>
2011-09-08 11:24:01 +0200

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppCloud::Application.config.secret_token = '023c1a16822ec47d86aebd709cce7d460f8a39df11ce2313b02798cc47a087e7c646144fc2bf45aa156510a2b45a24503bb6954ff152fa9976fc5d58db70b602'
